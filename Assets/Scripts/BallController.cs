144ae32a0c6983776a349c9d7269377c9a286aea
appishiwata <app.ishiwata@gmail.com>
2024-05-02 00:05:58 +0900

    ボール落下 ゴール処理

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,25 +1,34 @@
+using DG.Tweening;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] GameObject _ball;
     private Rigidbody _ballRigidbody;
     private Vector3 _lastMousePos;
     private readonly float _power = 5f;
     
+    [SerializeField] Transform _startTransform;
+    [SerializeField] Transform _goalTransform;
+    private Vector3 _startPosition;
+    private Vector3 _goalPosition;
+    //[SerializeField] ParticleSystem _goalEffect;
+    
     void Start()
     {
-        _ballRigidbody = _ball.GetComponent<Rigidbody>();
+        _ballRigidbody = GetComponent<Rigidbody>();
         bool isMouseDown = false;
+        
+        _startPosition = _startTransform.position;
+        _goalPosition = _goalTransform.position;
     
         // フリック操作で移動
         this.UpdateAsObservable().Subscribe(_ => 
         {
             if (Input.GetMouseButtonDown(0))
             {
-                this._lastMousePos = Input.mousePosition;
+                _lastMousePos = Input.mousePosition;
                 isMouseDown = true;
             }
 
@@ -36,5 +45,36 @@ public class BallController : MonoBehaviour
                 isMouseDown = false;
             }
         }).AddTo(this);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // ステージ外に出たらスタート地点に戻す
+        if (other.CompareTag("OutOfBounds"))
+        {
+            transform.position = _startPosition;
+            
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+
+        // ゴール範囲に入ったらゴール地点に移動
+        if (other.CompareTag("Goal"))
+        {
+            //goalEffect.Play();
+            
+            transform.DOMove(_goalPosition, 0.8f).SetDelay(0.25f);
+            
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
     }
 }
